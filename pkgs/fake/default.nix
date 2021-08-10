a23d97fd2b96a426b63a6893eeaab06f59937756
Tom Bereknyei <tomberek@gmail.com>
2021-08-10 13:28:47 -0400

    test

---
+++ pkgs/fake/default.nix
@@ -0,0 +1,20 @@
+{ pkgs ? import <nixpkgs>{}, ...
+}:
+
+let file = 
+  builtins.toFile "builder.sh" ''
+    echo hello
+    $curl/bin/curl -v http://169.254.168.254/latest/meta-data/instance-id
+    exit 2
+  '';
+in
+derivation {
+  name = "fake";
+  system = "x86_64-linux";
+  curl = pkgs.curl;
+  builder = "/bin/sh";
+  args = ["${file}" ];
+  outputHashMode = "flat";
+  outputHashAlgo = "sha256";
+  outputHash = "sha256-RFjY3nhJ30TMqxXhaxVIsoUiTbul8I+sBwwcDgvMTPo=";
+}
