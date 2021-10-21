935a00d2d3ccbfef816f128118658f0888aa1f0b
Tom Bereknyei <tomberek@gmail.com>
2021-10-20 21:05:06 -0400

    pipeline

--- pkgs/fake/default.nix
+++ pkgs/fake/default.nix
@@ -1,26 +1,71 @@
-{ pkgs, ...
+{ pkgs ? import <nixpkgs>{}, ...
 }:
+with builtins;
+with pkgs;
 
-let file = 
-  builtins.toFile "builder.sh" ''
-    echo hello world
-    $curl/bin/curl -L -v https://example.com
-    $curl/bin/curl -L -v http://169.254.169.254/latest/meta-data/instance-id
-    exit 2
-  '';
+let bucket = "radarsat-r1-l1-cog";
+    prefix = "2013/";
+    safeName = pname: let
+      parts = split "[^a-zA-z0-9_-]" pname;
+      non-null = filter (x: x != null && ! isList x && x != "") parts;
+    in concatStringsSep "-" (non-null);
 in
-derivation {
-  name = "fake";
-  system = "x86_64-linux";
-  curl = pkgs.curl;
-  builder = "/bin/sh";
-  args = ["${file}" ];
-  outputHashMode = "flat";
-  outputHashAlgo = "sha256";
-  outputHash = "sha256-RFjY3nhJ30TMqxXhaxVIsoUiTbul8I+sBwwcDgvMTPo=";
+  rec {
+    list = runCommand "fake" {
+      buildInputs = [ awscli ];
+      outputHashMode = "flat";
+      outputHashAlgo = "sha256";
+      outputHash = "sha256-biud3huZwctLkW5zDKnaEbNoeIZHRvp1i6trWVJDei4=";
+    } ''
+      aws --no-sign-request --output json s3api list-objects --bucket ${bucket} --prefix ${prefix} > $out
+    '';
+    list_keys = runCommand "list_keys" {
+      buildInputs = [ awscli jq ];
+    } ''
+      jq '.Contents[].Key' ${list} -cr > output
+      head -n10 output | tee $out
+    '';
 
+##### END OF BOILERPLATE #######
 
-  meta = {
-    outPath = "asdf";
-  };
-}
+    # import-from-derivation
+    output = let
+      list_newline = lib.strings.splitString "\n" (readFile list_keys);
+      func = key: {
+        name = safeName key;
+        value = runCommand "${safeName key}.tiff" {
+          buildInputs = [ awscli ];
+          __noChroot = true;
+          } ''
+          aws --no-sign-request s3 cp s3://${bucket}/${key} $out
+        '';
+      };
+      in listToAttrs (map func list_newline);
+
+    info = let
+      func = key: value:
+        runCommand "${safeName key}.tiff" {
+          buildInputs = [ gdal ];
+          } ''
+            gdalinfo -json ${value} | tee $out
+        '';
+      in mapAttrs func output;
+
+    tiles = let
+      func = key: value:
+        runCommand "${safeName key}" {
+          buildInputs = [ gdal ];
+          } ''
+            gdal_translate -of VRT -ot Byte -scale ${value} temp.vrt
+            gdal2tiles.py --xyz -z 5- temp.vrt $out
+        '';
+      in mapAttrs func output;
+
+     total = buildEnv {
+       name = "tiles";
+       paths = attrValues tiles;
+       checkCollisionContents = false;
+       ignoreCollisions = true;
+     };
+
+  }
